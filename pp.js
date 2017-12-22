657cf9a8953539fcd4a0bfebf98de3d99516cc6c
kartikmat <kmathur@usc.edu>
2017-12-22 12:06:16 -0800

    Updated Closing
    The Close linke works for the modal

--- pp.js
+++ pp.js
@@ -1,5 +1,15 @@
-if(true)
+var x=9;
+while (x>0) 
 {
-   document.getElementById("Reference").innerHTML = "Execute Script";
+ console.log(x);
+ x--;   
+}
+var y={firstname:"John", 
+            lastname:"Doe"};
+
+
+function myfunction()
+{
+ return 5;
 }
 
