1c168aac44332a590300208784d5fc3f6a11b03c
kartikmat <kmathur@usc.edu>
2018-07-21 18:19:22 +0530

    Testing on sample

--- caption-searcher/main.js
+++ caption-searcher/main.js
@@ -9,6 +9,7 @@ const signoutButton = document.getElementById('signout-button');
 const content = document.getElementById('content');
 const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
+const keyword = document.getElementById('keyword');
 const videoContainer = document.getElementById('video-container');
 
 const defaultChannel = 'TheSw1tcher';
@@ -120,18 +121,29 @@ function getChannel(channel) {
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+function contains(key,caption)
+{
+    var n = caption.search(key);
+    if (n==-1) {
+        console.log("Is not in the captions");
+        
+        return false;
+    }
+    console.log("In the captions");
+    return true;
+}
 
 function parseXML(xml){
     console.log("Parsing");
-    console.log(xml.transcripts);
-    console.log(xml.texts);
-   
-    console.log(xml.getElementsByTagName(transcript));
-    console.log(xml.getElementsByTagName(text));
-    console.log(xml.getElementsByTagName(transcript));
-    
-   
-    
+    var caption="";
+    for (let index = 0; index < xml.childNodes[0].childNodes.length; index++) {
+        
+        console.log(xml.childNodes[0].childNodes[index].innerHTML);
+        caption+=xml.childNodes[0].childNodes[index].innerHTML;
+        
+       
+    }
+  return caption;
 }
 
 function makeRequest(videoId)
@@ -142,13 +154,21 @@ function makeRequest(videoId)
             type: "POST",
             url: "https://video.google.com/timedtext?lang=en&v=5MgBikgcWnY"
           }).done(function (response) {
-            console.log(response);
+           
             console.dir(response);
-            parseXML(response);
+            if(contains(keyword,parseXML(response)))
+            {
+              return true;
+            }
+            else
+            {
+              return false;
+            }
           }).fail(function (response) {
             console.log();
           });
 }
+
 
 function requestVideoPlaylist(playlistId) {
   const requestOptions = {
@@ -170,6 +190,8 @@ function requestVideoPlaylist(playlistId) {
         const videoId = item.snippet.resourceId.videoId;
         
         makeRequest(videoId);
+    
+      
         output += `
           <div class="col s3">
           <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
