b55f6c1eb4eefa1a9843ee9753b56273e2a39afd
kartikmat <kmathur@usc.edu>
2021-01-20 21:54:50 -0800

    Updates

---
+++ precache-manifest.e824c9f62f0e3d1125c0e6fce744c412.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad769d34e2b0d13d1d4e8e6e6c16fe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bf48977923f7b6b98c7",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "b0fa9f23d61ab5ea17d1",
+    "url": "/static/js/2.d016f94f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d016f94f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf48977923f7b6b98c7",
+    "url": "/static/js/main.0adcefa4.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "cd695daef345da4a91ed0470dcdd7259",
+    "url": "/static/media/Default.cd695dae.jpg"
+  },
+  {
+    "revision": "cf8d98ef4c0f4a256edb29ed359e0108",
+    "url": "/static/media/resume.cf8d98ef.pdf"
+  }
+]);
